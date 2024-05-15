18ae42fd9753f4e2903afe2052ad0605bde66ade
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2024-05-15 18:58:48 +0900

    feat: mocking한 데이터를 실행하는 실행함수 구현

---
+++ src/mock/index.js
@@ -0,0 +1,17 @@
+import { setupWorker } from "msw/browser";
+import main from "../api/get-main";
+import detail from "../api/get-detail";
+
+const initMocks = () => {
+  try {
+    const mockHandler = [main, detail].map(handler => handler());
+    const worker = setupWorker(...mockHandler);
+    worker.start();
+    return true;
+  } catch (error) {
+    if (import.meta.env.DEV) console.error(`worker error: ${error}`);
+    return false;
+  }
+};
+
+export default initMocks;
