407f3aee834e4bfd929509980d48b0c3a44f0dda
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2024-05-17 16:46:31 +0900

    chore: 불필요한 주석 제거

--- src/main.js
+++ src/main.js
@@ -1,11 +1,3 @@
 import initMocks from "./mock";
-import axios from "axios";
 
 initMocks();
-
-const data = async () => {
-  const data = await axios.get("/main");
-  console.log(data.data);
-};
-
-data();
--- src/mock/handler.js
+++ src/mock/handler.js
@@ -4,15 +4,3 @@ const handler = (method, path, data, callback) =>
   rest[method](path, (req, res, ctx) => res(ctx.json(callback(req, data))));
 
 export default handler;
-
-// const handlers = [
-//   http.get("/main", () => {
-//     return HttpResponse.json({
-//       id: "c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d",
-//       firstName: "John",
-//       lastName: "Maverick",
-//     });
-//   }),
-// ];
-
-// export default handlers;
