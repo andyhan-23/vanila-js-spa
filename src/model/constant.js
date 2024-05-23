bd7ba3ad1f2db31c76fe49192d239ef56587e7ae
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2024-05-23 21:44:59 +0900

    feat: 테스트 코드 작성

--- src/model/constant.js
+++ src/model/constant.js
@@ -1,7 +1,7 @@
 export const URL = Object.freeze({
   main: "/main",
   detail: "/detail",
-  base: "tossblog/v1",
+  base: "/tossblog/v1",
 });
 
-export const FIX_PATH = "http//localhost";
+export const FIX_PATH = "http://localhost";
--- src/model/request.js
+++ src/model/request.js
@@ -6,10 +6,18 @@ const instance = axios.create({
   baseURL: URL.base,
 });
 
+const splitUrl = config => {
+  let temp_url = config.url.split("/");
+  return `/${temp_url[1]}`;
+};
+
 instance.interceptors.request.use(config => {
-  if (config.url.startsWith(URL.main) || config.url.startsWith(URL.detail)) {
+  const currentUrl = splitUrl(config);
+  if (URL.main === currentUrl || URL.detail === currentUrl) {
     initMocks();
     config.baseURL = null;
+  } else {
+    throw new Error(`${config.url}는 잘못된 요청입니다.`);
   }
   return config;
 });
@@ -17,19 +25,23 @@ instance.interceptors.request.use(config => {
 instance.interceptors.response.use(async response => {
   try {
     const result = await response.data;
+    if (!result) {
+      throw new Error("데이터가 없습니다.");
+    }
     return result;
   } catch (error) {
+    const currentUrl = splitUrl(response.config);
     if (import.meta.env.DEV) {
-      if (response.config.url.startsWith(URL.main))
+      if (URL.main === currentUrl)
         console.error(
           `${URL.main}요청에 대한 응답을 받는 도중 에러 발생:${error}`,
         );
-      else if (response.config.url.startsWith(URL.detail))
+      else if (URL.detail === currentUrl)
         console.error(
           `${URL.detail}요청에 대항 응답을 받는 도중 에러 발생:${error}`,
         );
     }
-    return Promise.reject(error);
+    return null;
   }
 });
 
